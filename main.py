fb9203e6fbe8b8bda9e7791e0e37b4baa62e431f
Mateus Carvalho <mateus.carvalho.ti@gmail.com>
2020-11-22 19:06:28 +0000

    1

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
- return '<h1>Hello World!</h1>'
- 
+ return '<h1>Hello Word!</h1>'
+
+@app.route('/unifran')
+def index2():
+ return '<h2>Universidade de Franca</h2>'
+
 if __name__ == "__main__":
  port = int(os.environ.get("PORT", 5000))
  app.run(host='0.0.0.0', port=port)
